a982baf6c96bc76af96ad75140912cc621e897d8
a-jinjin <99878175+a-jinjin@users.noreply.github.com>
2022-05-19 17:49:35 +0900

    layout-40예제/ 배열의 위치변경

---
+++ algorithm-practice/array-test.js
@@ -0,0 +1,16 @@
+const testArr = [1, 2, 3, 4, 5];
+
+testArr.push(testArr[0]);
+// console.log(testArr);
+testArr.shift(testArr[0]);
+console.log(testArr);
+
+testArr.push(testArr[0]);
+// console.log(testArr);
+testArr.shift(testArr[0]);
+console.log(testArr);
+
+testArr.push(testArr[0]);
+// console.log(testArr);
+testArr.shift(testArr[0]);
+console.log(testArr);
